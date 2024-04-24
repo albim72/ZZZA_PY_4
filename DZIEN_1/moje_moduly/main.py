2808bce0b544df82afaf9ba07d1fe53682e105c1
albim72 <marcin.albiniak72@gmail.com>
2024-04-24 14:58:09 +0200

    Update main.py

--- DZIEN_1/moje_moduly/main.py
+++ DZIEN_1/moje_moduly/main.py
@@ -2,7 +2,12 @@
 # import dane as dn
 from dane import nrfilii as filia
 from dane import book as bk
+from funkcje.funkcjab import czytaj_liste,czytaj_slownik
 
 print("____________ wyświetlanie bezpośrednie _______________")
 print(filia)
 print(bk)
+
+print("____________ wyświetlanie za pomocą funkcji _______________")
+czytaj_liste(filia)
+czytaj_slownik(bk)
