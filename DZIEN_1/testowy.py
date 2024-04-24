6cc8812b59ed245b4a003bc902187d2d4fdab5ca
albim72 <marcin.albiniak72@gmail.com>
2024-04-24 11:06:23 +0200

    Update testowy.py

--- DZIEN_1/testowy.py
+++ DZIEN_1/testowy.py
@@ -7,3 +7,18 @@ def xy(a:int,b:int)->int:
 print(xy(4,6))
 print(xy(1.2,0.5434))
 print(xy(True,5))
+
+
+print((lambda g:g**4)(56))
+
+b = lambda x,v,m=1:(x+v)*m
+print(b(8,4,2))
+print(b(8,4))
+
+def policz(n):
+    return lambda a:a*n
+
+print(policz(5)(9))
+
+
+
