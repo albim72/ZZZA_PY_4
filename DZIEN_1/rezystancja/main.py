d33a333915086866491f0bf755808f801d3e91f7
albim72 <marcin.albiniak72@gmail.com>
2024-04-24 15:38:46 +0200

    Update main.py

--- DZIEN_1/rezystancja/main.py
+++ DZIEN_1/rezystancja/main.py
@@ -1,4 +1,5 @@
 from oldresistor import OldResistor
+from resistor import Resistor
 
 r0 = OldResistor(10.2E2)
 print(f"________________ klasa {r0.__class__.__name__} ______________")
@@ -6,3 +7,10 @@ print(r0)
 print(r0.get_ohms())
 r0.set_ohms(5.5E2)
 print(r0.get_ohms())
+
+
+r1 = Resistor(5.01E3)
+print(f"________________ klasa {r1.__class__.__name__} ______________")
+print(r1.ohms)
+r1.ohms = 2.2E3
+print(r1.ohms)
