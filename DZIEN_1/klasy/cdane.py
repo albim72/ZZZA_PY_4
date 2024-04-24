3759fc50b1a17a61a1547d42573c7d72df65d399
albim72 <marcin.albiniak72@gmail.com>
2024-04-24 15:08:18 +0200

    Create cdane.py

---
+++ DZIEN_1/klasy/cdane.py
@@ -0,0 +1,11 @@
+from funkcje.funkcjab import *
+
+class CDane:
+    def __init__(self,lista,slownik):
+        self.lista = lista
+        self.slownik = slownik
+        
+    def __new__(cls, *args, **kwargs):
+        pass
+
+        
