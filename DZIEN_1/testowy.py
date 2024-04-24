a289120aaea0e9ab763ab8dba69f4e4737881f9e
albim72 <marcin.albiniak72@gmail.com>
2024-04-24 11:19:55 +0200

    Update testowy.py

--- DZIEN_1/testowy.py
+++ DZIEN_1/testowy.py
@@ -20,5 +20,27 @@ def policz(n):
 
 print(policz(5)(9))
 
+print("funkcje wyższego rzędu...")
 
+num = [67,9,-45,2,0,11,5,23,7,90,3,-3,-98,101,46,8,2,-3,1]
+
+parzyste = list(filter(lambda x:x%2==0,num))
+print(parzyste)
+
+cube = list(map(lambda x:x**3,num))
+print(cube)
+
+print("___________ własna funkcja wyższgo rzędu _____________")
+
+def witaj(imie):
+    return f"Miło Cię widzieć {imie}!"
+
+def konkurs(imie,miasto,punkty):
+    return f"uczestnik konkursu -> {imie}, miasto: {miasto}, liczba punktów: {punkty}"
+
+def osoba(funkcja,*args):
+    return funkcja(*args)
+
+print(osoba(witaj,"Olaf"))
+print(konkurs("Olga","Kraków",56))
 
