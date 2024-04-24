b1320c72c18d623bc387800dd47161c5edca6454
albim72 <marcin.albiniak72@gmail.com>
2024-04-24 14:49:48 +0200

    Create main.py

---
+++ DZIEN_1/moje_moduly/main.py
@@ -0,0 +1,8 @@
+# import dane
+# import dane as dn
+from dane import nrfilii as filia
+from dane import book as bk
+
+print("____________ wyświetlanie bezpośrednie _______________")
+print(filia)
+print(bk)
