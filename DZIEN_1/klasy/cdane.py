bf8bbf4f9c269d5c9ed332e9b1f55f63e1da2763
albim72 <marcin.albiniak72@gmail.com>
2024-04-24 15:14:43 +0200

    Update cdane.py

--- DZIEN_1/klasy/cdane.py
+++ DZIEN_1/klasy/cdane.py
@@ -4,8 +4,15 @@ class CDane:
     def __init__(self,lista,slownik):
         self.lista = lista
         self.slownik = slownik
-        
-    def __new__(cls, *args, **kwargs):
-        pass
 
-        
+    # def __new__(cls, *args, **kwargs):
+    #     return object.__new__(CDane)
+
+    def __repr__(self):
+        return "To jest obiekt klasy służącej do przetwarzania list oraz słowników"
+    
+    def czytaj_l(self):
+        return czytaj_liste(self.lista)
+    
+    def czytaj_s(self):
+        return czytaj_slownik(self.slownik)
