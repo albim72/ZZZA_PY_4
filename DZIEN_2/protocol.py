acf6e404f03f22633c417e046391c8762a03a80e
albim72 <marcin.albiniak72@gmail.com>
2024-04-25 14:02:46 +0200

    Create protocol.py

---
+++ DZIEN_2/protocol.py
@@ -0,0 +1,29 @@
+from typing import List,Protocol
+
+class Item(Protocol):
+    quantity:float
+    price:float
+
+class Product:
+    def __init__(self,name:str,quantity:float,price:float):
+        self.name = name
+        self.quantity = quantity
+        self.price = price
+
+
+def calculate_total(items:List[Item])->float:
+    return sum([item.quantity*item.price for item in items])
+
+total = calculate_total(
+    [
+        Product('masło',3,6.4),
+        Product('ser kozi',1,13.5),
+        Product('papryka',1.5,11.99),
+        Product('woda mineralna',12,1.89)
+    ]
+)
+
+print(f"do zapłaty: {total:.2f} zł")
+
+
+
