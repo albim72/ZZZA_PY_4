a0c3da1a79cafa28bcf8dd4cda38f1a243c5d199
albim72 <marcin.albiniak72@gmail.com>
2024-04-25 09:25:43 +0200

    Update resistor.py

--- DZIEN_1/rezystancja/resistor.py
+++ DZIEN_1/rezystancja/resistor.py
@@ -1,5 +1,6 @@
 class Resistor:
-    def __init__(self,ohms):
+    def __init__(self,ohms,width):
         self.ohms = ohms
+        self.width = width
         self.voltage = 0
         self.current = 0
