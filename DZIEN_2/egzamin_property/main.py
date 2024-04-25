67c10dc3724803c60995725aabfe2e30156e281a
albim72 <marcin.albiniak72@gmail.com>
2024-04-25 11:00:50 +0200

    Update main.py

--- DZIEN_2/egzamin_property/main.py
+++ DZIEN_2/egzamin_property/main.py
@@ -23,3 +23,13 @@ s3f.math_grade = 34
 s3f.writing_grade = 55
 s3f.science_grade = 43
 print(f"pierwsze podejście\nmath:{s3f.math_grade}\nwriting:{s3f.writing_grade}\nscience:{s3f.science_grade}")
+print("__________________________________")
+s3s = ExamD()
+s3s.math_grade = 77
+s3s.writing_grade = 65
+s3s.science_grade = 89
+print(f"drugie podejście\nmath:{s3s.math_grade}\nwriting:{s3s.writing_grade}\nscience:{s3s.science_grade}")
+print("__________________________________")
+print(f"ARCHIWUM:pierwsze podejście\nmath:{s3f.math_grade}\nwriting:{s3f.writing_grade}\nscience:{s3f.science_grade}")
+print(s3f==s3s)
+print(s3f.math_grade == s3s.math_grade)
