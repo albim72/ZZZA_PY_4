c1a078bace9a84ccee1eb0ea13e2b67657b0e98f
albim72 <marcin.albiniak72@gmail.com>
2024-04-25 11:31:42 +0200

    Update weakgrade.py

--- DZIEN_2/egzamin_property/weakgrade.py
+++ DZIEN_2/egzamin_property/weakgrade.py
@@ -1,13 +1,15 @@
 from weakref import WeakKeyDictionary
 
 class Grade:
-    def __init__(self):
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
         self._values = WeakKeyDictionary()
 
     def __get__(self, instance, owner):
         if instance is None:
             return self
-        return self._values.get(instance,0)
+        return self._values.get(instance,0),self.a,self.b
 
     def __set__(self, instance, value):
         if not (0 <= value <= 100):
@@ -15,6 +17,6 @@ class Grade:
         self._values[instance] = value
 
 class ExamC:
-    math_grade = Grade()
-    writing_grade = Grade()
-    science_grade = Grade()
+    math_grade = Grade(3,5)
+    writing_grade = Grade(7,3)
+    science_grade = Grade(1,4)
