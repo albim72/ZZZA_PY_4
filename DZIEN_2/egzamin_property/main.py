6ea19001c7ed4d1822206bdd21255e9b3639d15b
albim72 <marcin.albiniak72@gmail.com>
2024-04-25 10:21:26 +0200

    Update main.py

--- DZIEN_2/egzamin_property/main.py
+++ DZIEN_2/egzamin_property/main.py
@@ -1,7 +1,16 @@
 from homework import Homework
+from exam import Exam
 
 print("____________ HOMEWORK _______________")
 s1 = Homework()
 s1.grade = 97
 assert s1.grade == 97
 print(f'ocena: {s1.grade}')
+
+
+print("____________ EXAM _______________")
+s2 = Exam()
+s2.writing_grade=88
+s2.math_grade=75
+
+print(f'oceny -> writing: {s2.writing_grade}, math: {s2.math_grade}')
