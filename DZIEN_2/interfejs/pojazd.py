626d7e48725633f1162984c5947868592a67777c
albim72 <marcin.albiniak72@gmail.com>
2024-04-25 13:22:07 +0200

    Create pojazd.py

---
+++ DZIEN_2/interfejs/pojazd.py
@@ -0,0 +1,14 @@
+from ipojazd import IPojazd
+
+class Pojazd(IPojazd):
+    def spalanie(self, odl, litry):
+        return litry*100/odl
+
+    def kosztyprzejazdu(self, odl, litry, cena_l):
+        return self.spalanie(odl,litry)*(odl/100)*cena_l
+
+    def opis(self, msg):
+        return f"spalanie {msg}"
+
+    def multipojazd(self, odl, litry, msg):
+        return super().multipojazd(odl, litry, msg) + " - dane aktualne"
