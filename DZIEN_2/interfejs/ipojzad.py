8f69603f5cf873ad02046afda836a15832a1efe1
albim72 <marcin.albiniak72@gmail.com>
2024-04-25 13:17:44 +0200

    Create ipojzad.py

---
+++ DZIEN_2/interfejs/ipojzad.py
@@ -0,0 +1,15 @@
+from abc import ABCMeta,abstractmethod
+
+class IPojazd(metaclass=ABCMeta):
+    @abstractmethod
+    def spalanie(self,odl,litry):raise NotImplementedError
+    
+    @abstractmethod
+    def kosztyprzejazdu(self,odl,litry,cena_l):raise NotImplementedError
+    
+    @abstractmethod
+    def opis(self,msg):raise NotImplementedError
+    
+    @abstractmethod
+    def multipojazd(self,odl,litry,msg):
+        return self.opis(msg),self.spalanie(odl,litry)
