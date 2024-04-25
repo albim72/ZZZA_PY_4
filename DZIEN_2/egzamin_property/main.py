6f11bd0ba99b287e5c7615d286cac14e50539a87
albim72 <marcin.albiniak72@gmail.com>
2024-04-25 11:18:10 +0200

    Update main.py

--- DZIEN_2/egzamin_property/main.py
+++ DZIEN_2/egzamin_property/main.py
@@ -1,6 +1,7 @@
 from homework import Homework
 from exam import Exam
 from grade import ExamD
+from weakgrade import ExamC
 
 print("____________ HOMEWORK _______________")
 s1 = Homework()
@@ -33,3 +34,22 @@ print("__________________________________")
 print(f"ARCHIWUM:pierwsze podejście\nmath:{s3f.math_grade}\nwriting:{s3f.writing_grade}\nscience:{s3f.science_grade}")
 print(s3f==s3s)
 print(s3f.math_grade == s3s.math_grade)
+
+
+print("____________ EXAMC _______________")
+
+s4f = ExamC()
+s4f.math_grade = 34
+s4f.writing_grade = 55
+s4f.science_grade = 43
+print(f"pierwsze podejście\nmath:{s4f.math_grade}\nwriting:{s4f.writing_grade}\nscience:{s4f.science_grade}")
+print("__________________________________")
+s4s = ExamC()
+s4s.math_grade = 77
+s4s.writing_grade = 65
+s4s.science_grade = 89
+print(f"drugie podejście\nmath:{s4s.math_grade}\nwriting:{s4s.writing_grade}\nscience:{s4s.science_grade}")
+print("__________________________________")
+print(f"ARCHIWUM:pierwsze podejście\nmath:{s4f.math_grade}\nwriting:{s4f.writing_grade}\nscience:{s4f.science_grade}")
+print(s4f==s4s)
+print(s4f.math_grade == s4s.math_grade)
