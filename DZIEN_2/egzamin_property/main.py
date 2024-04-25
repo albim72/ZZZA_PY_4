6232febab03bc04cfcc8a099f9ef586e8f135af9
albim72 <marcin.albiniak72@gmail.com>
2024-04-25 10:53:44 +0200

    Update main.py

--- DZIEN_2/egzamin_property/main.py
+++ DZIEN_2/egzamin_property/main.py
@@ -1,5 +1,6 @@
 from homework import Homework
 from exam import Exam
+from grade import ExamD
 
 print("____________ HOMEWORK _______________")
 s1 = Homework()
@@ -14,3 +15,11 @@ s2.writing_grade=88
 s2.math_grade=75
 
 print(f'oceny -> writing: {s2.writing_grade}, math: {s2.math_grade}')
+
+print("____________ EXAMD _______________")
+
+s3f = ExamD()
+s3f.math_grade = 34
+s3f.writing_grade = 55
+s3f.science_grade = 43
+print(f"pierwsze podejście\nmath:{s3f.math_grade}\nwriting:{s3f.writing_grade}\nscience:{s3f.science_grade}")
