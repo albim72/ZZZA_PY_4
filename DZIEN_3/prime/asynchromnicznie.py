321921c1759bb461dd67d585e0981c4c5f4820f0
albim72 <marcin.albiniak72@gmail.com>
2024-04-26 14:01:59 +0200

    Update asynchromnicznie.py

--- DZIEN_3/prime/asynchromnicznie.py
+++ DZIEN_3/prime/asynchromnicznie.py
@@ -3,14 +3,22 @@ import time
 import concurrent.futures
 
 def heavy_function(params):
-    return find_prime_numbers_sum(*params)
+    t1 = time.time()
+    w = find_prime_numbers_sum(*params)
+    t2 = time.time()
+    tc=t2-t1
+    return w,tc
 
 def asynchr():
     nb = [(1, 10_000), (3, 50_000), (5000, 100_000), (4, 900), (800, 15_100)]
     start_time = time.time()
     with concurrent.futures.ProcessPoolExecutor(max_workers=6) as executor:
+        # st = time.time()
         result = executor.map(heavy_function,nb)
-    print(list(result))
-    
+        # kn = time.time()
+        # tm=kn-st
+        # print(tm)
+        print(list(result))
+
     end_time = time.time()
     print(f'całkowity czas wykonania zadania asynchronicznie: {end_time - start_time} s')
